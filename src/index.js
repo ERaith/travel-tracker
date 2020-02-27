8352facd1f627f46a4d4fa5b284a6ad9d3edeff1
Erik Raith <raithhtiar@gmail.com>
2020-02-27 14:29:58 -0700

    [init] Add file structure for SCSS files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,35 @@
 // Do not delete or rename this file ********
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
+import $ from "jquery";
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import "./css/application.scss";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import "./images/turing-logo.png";
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let user = {
+  role: "anonymous"
+};
+
+
+//Initialize User
+
+//
+
+
+
+
+// Show div html based on role
+if (user.role == "anonymous") {
+  $("div.anonymous").show();
+}
+
+if (user.role == "end_user") {
+  $("div.end_user").show();
+}
+
+if (user.role == "manager") {
+  $("div.manager").show();
+}
