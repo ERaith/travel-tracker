a878c264379dbfaf4cb8dc3172bc75d765cba682
Erik Raith <raithhtiar@gmail.com>
2020-02-28 09:24:47 -0700

    [teat][fix] Add user control to DOM

--- src/domUpdate.js
+++ src/domUpdate.js
@@ -1,33 +1,16 @@
-const loginView = require("./views/login");
+const {generateLogin} = require("./views/login");
 import $ from "jquery";
-// const $ = require('jquery');
 
 class DomUpdate {
   constructor() {
     this.body = $("body");
-    // this.searchField = $(".search-bar");
   }
-
   greetUser(user) {}
+
   createDOMBindings() {}
 
   loginForm(user) {
-    this.body.append(loginView.html);
-    let loginButton = $("button");
-    console.log(loginButton)
-    let userName = $("input")
-
-    loginButton.click(function(event) {
-      event.preventDefault();
-      var name = $("#userName").val();
-      var password = $("#password").val();
-      user.updateName(name);
-      user.login(password);
-      console.log(name);
-      console.log(password);
-      return false;
-     
-    });
+    generateLogin(user,this.body);
   }
 }
 
--- src/views/login.js
+++ src/views/login.js
@@ -1,14 +1,27 @@
-let loginHtml = `
-<div class="login-page">
-  <div class="form">
-    <form class="login-form">
-      <input id="userName" type="text" placeholder="username"/>
-      <input id="password" type="password" placeholder="password"/>
-      <button>login</button>
-    </form>
-  </div>
-</div>
-`;
+import $ from "jquery";
+export function generateLogin(user,body) {
+  let loginHtml = `
+    <div class="login-page">
+      <div class="form">
+        <form class="login-form">
+          <input id="userName" type="text" placeholder="username"/>
+          <input id="password" type="password" placeholder="password"/>
+          <button>login</button>
+        </form>
+      </div>
+    </div>
+    `;
 
+  $(body).append(loginHtml);
 
-module.exports = { html: loginHtml };
+  let loginButton = $("button");
+
+  loginButton.click(function(event) {
+    event.preventDefault();
+    var name = $("#userName").val();
+    var password = $("#password").val();
+    user.updateName(name);
+    console.log(user.login(password));
+    return false;
+  });
+}
