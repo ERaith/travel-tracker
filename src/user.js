005aeee4067d9a825b57b46de160be11e522209f
Erik Raith <raithhtiar@gmail.com>
2020-03-03 23:12:16 -0700

    [hotfix]

--- src/user.js
+++ src/user.js
@@ -9,8 +9,14 @@ class User {
   }
 
   updateTotalCost(filteredData,type) {
+    console.log(type)
+    if(type === 'Spent'){
+      type = 'cost';
+    } else {
+      type = 'adminFee';
+    }
     let total = Math.round(filteredData.reduce((sum,trip) => {
-      sum = sum + (trip.adminFee ||trip.cost)
+      sum = sum + trip[type]
       return sum;
     },0))
     return total.toLocaleString("us-US", {
